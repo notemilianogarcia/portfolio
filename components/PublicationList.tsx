2dc0665a6c5f5f10b01cdefb73dc5e0298ecea76
Emiliano Garcia <emichel2001@gmail.com>
2026-01-30 15:31:00 -0500

    Make publication cards more enticing
    - Update 'Coming Soon' abstract to encourage clicks with research background teaser
    - Enable abstract display in PublicationList component
    - Cards now show enticing preview text to draw visitors to detailed pages

--- components/PublicationList.tsx
+++ components/PublicationList.tsx
@@ -9,7 +9,7 @@ export function PublicationList({ publications, className }: PublicationListProp
   return (
     <div className={`flex flex-col gap-8 ${className ?? ""}`}>
       {publications.map((pub) => (
-        <PublicationCard key={pub.slug} publication={pub} />
+        <PublicationCard key={pub.slug} publication={pub} showAbstract={true} />
       ))}
     </div>
   );
